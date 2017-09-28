58c42cc5185c0bf05a6fe7ece29079c059de3d4b
Dorian Scholz <DorianScholz@gmx.de>
2017-09-28 17:07:49 +0200

    simplify finalizers

--- src/finalizers/abstractFinalizer.py
+++
@@ -1,9 +0,0 @@
-import abc
-
-
-class AbstractFinalizer(object):
-    __metaclass__ = abc.ABCMeta
-
-    @abc.abstractmethod
-    def final_action():
-        pass
--- src/finalizers/saveGeotiff.py
+++ src/finalizers/saveGeotiff.py
@@ -1,17 +1,11 @@
-from abstractFinalizer import AbstractFinalizer
 import rospy
 from std_msgs.msg import String
 
 from ci_tools.helpers.ci_log import CiLog
 
 
-class SaveGeotiff(AbstractFinalizer):
-    def __init__(self):
-        print "init"
-
-    @staticmethod
-    def final_action():
-        CiLog.info("[finalizers.SaveGeotiff]: Saving GeoTiff...")
-        sys_command_publisher = rospy.Publisher("syscommand", String, queue_size=5)
-        geotiff_string = "savegeotiff"
-        sys_command_publisher.publish(geotiff_string)
+def save_geotiff():
+    CiLog.info("[finalizers.SaveGeotiff]: Saving GeoTiff...")
+    sys_command_publisher = rospy.Publisher("syscommand", String, queue_size=5)
+    geotiff_string = "savegeotiff"
+    sys_command_publisher.publish(geotiff_string)
